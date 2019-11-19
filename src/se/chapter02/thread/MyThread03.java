b7d9c6917f3d7565cd0d3279102bb6546ef69d8e
1352895716 <1352895716@qq.com>
2019-11-19 09:53:13 +0800

    第一次使用idea添加文件到远程仓库

---
+++ src/se/chapter02/thread/MyThread03.java
@@ -0,0 +1,16 @@
+package se.chapter02.thread;
+
+/**
+ * @ClassName: MyThread03
+ * @Description: TODO
+ * @Autor:13528
+ * @Date: 2019/11/19 9:51
+ * @Version 1.0
+ **/
+public class MyThread03 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
